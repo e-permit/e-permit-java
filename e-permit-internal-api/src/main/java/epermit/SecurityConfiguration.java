bc8960876b7abae7d98f9ad06a2933d6a9cf1d30
ademcaglin <adem.caglin@gmail.com>
2022-06-13 14:10:39 +0300

    Change WebMvcConfigurerAdapter to SecurityFilter

---
+++ e-permit-internal-api/src/main/java/epermit/SecurityConfiguration.java
@@ -0,0 +1,32 @@
+package epermit;
+
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfiguration {
+    @Value("${epermit.admin-password}")
+    private String adminPassword;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.cors().and().csrf().disable().authorizeRequests().anyRequest().authenticated().and()
+                .httpBasic();
+
+        return http.build();
+    }
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService() {
+        UserDetails user = User.withUsername("admin").password("{noop}" + adminPassword + "1")
+                .roles("ADMIN").build();
+        return new InMemoryUserDetailsManager(user);
+    }
+}
--- e-permit-public-api/src/main/java/epermit/SecurityConfiguration.java
+++ e-permit-public-api/src/main/java/epermit/SecurityConfiguration.java
@@ -1,14 +1,17 @@
 package epermit;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
-    @Override
-    protected void configure(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity.csrf().disable().authorizeRequests().anyRequest().permitAll();
+public class SecurityConfiguration {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf().disable().authorizeRequests().anyRequest().permitAll();
+        return http.build();
     }
 
 }
+
