217354493cc1ab0716c0cecde23e0fca95762db4
ademcaglin <adem.caglin@gmail.com>
2022-09-16 11:19:42 +0300

    Fix http status code

--- e-permit/src/main/java/epermit/ledgerevents/LedgerEventUtil.java
+++ e-permit/src/main/java/epermit/ledgerevents/LedgerEventUtil.java
@@ -162,7 +162,7 @@ public class LedgerEventUtil {
         HttpEntity<Map<String, Object>> request = new HttpEntity<>(event.getContent(), headers);
         ResponseEntity<?> result =
                 restTemplate.postForEntity(event.getUri(), request, Object.class);
-        if (result.getStatusCode() == HttpStatus.ACCEPTED) {
+        if (result.getStatusCode() == HttpStatus.OK) {
             return true;
         }
         MDC.put("epermitSendError", "SEND_EPERMIT_EVENT_ERROR");
