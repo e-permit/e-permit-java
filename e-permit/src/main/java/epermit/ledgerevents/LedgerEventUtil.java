8fa595f71147da32ae82459b765b149d7c30e4b0
ademcaglin <adem.caglin@gmail.com>
2024-06-27 12:53:47 +0300

    Fix conflicts

--- e-permit/src/main/java/epermit/ledgerevents/LedgerEventUtil.java
+++ e-permit/src/main/java/epermit/ledgerevents/LedgerEventUtil.java
@@ -54,21 +54,13 @@ public class LedgerEventUtil {
     }
 
     @SneakyThrows
-    public <T extends LedgerEventBase> void persistAndPublishEvent(T event){
-        persistAndPublishEvent(event, true);
-    }
-
-    @SneakyThrows
-    public <T extends LedgerEventBase> void persistAndPublishEvent(T event, boolean handleEvent) {
+    public <T extends LedgerEventBase> void persistAndPublishEvent(T event) {
         CreatedEvent createdEvent = new CreatedEvent();
         createdEvent.setEventId(event.getEventId());
         createdEvent.setSent(false);
         createdEventRepository.save(createdEvent);
-        if (handleEvent) {
-            String proof = createProof(event);
-            handleEvent(event, proof);
-        }
-
+        String proof = createProof(event);
+        handleEvent(event, proof);
         publishAppEvent(createdEvent);
     }
 
@@ -163,31 +155,9 @@ public class LedgerEventUtil {
         String jws = proofArr[0] + "." + payloadBase64 + "." + proofArr[1];
         Boolean isValid = jwsUtil.validateJws(jws);
         if (!isValid) {
-            return VerifyProofResult.fail("UNAUTHORIZED");
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Invalid jws");
         }
-        return VerifyProofResult.success(proof);
-    }
-
-    @SneakyThrows
-    public ResponseEntity<?> sendEvent(LedgerEventCreated event) {
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_JSON);
-        headers.add("Authorization", "Bearer " + event.getProof());
-        HttpEntity<Map<String, Object>> request = new HttpEntity<>(event.getContent(), headers);
-        ResponseEntity<?> result = restTemplate.postForEntity(event.getUri(), request, Object.class);
-        return result;
-        /*
-         * if (result.getStatusCode() != HttpStatus.OK) {
-         * ApiErrorResponse error = (ApiErrorResponse)result.getBody();
-         * if(error != null &&
-         * error.getDetails().get("errorCode").equals("EVENT_ALREADY_EXISTS")){
-         * 
-         * }
-         * log.error(GsonUtil.getGson().toJson(result.getBody()));
-         * return false;
-         * }
-         * return true;
-         */
+        return proof;
     }
 
 }
