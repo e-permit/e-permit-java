20ed827e23c0c8830f637a7376ca8de0fe663b5b
ademcaglin <adem.caglin@gmail.com>
2024-07-31 13:34:09 +0300

    Add crossorigin to index

--- e-permit-public-api/src/main/java/epermit/controllers/IndexController.java
+++ e-permit-public-api/src/main/java/epermit/controllers/IndexController.java
@@ -3,6 +3,7 @@ package epermit.controllers;
 import java.util.List;
 
 import org.springframework.boot.info.BuildProperties;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -13,6 +14,7 @@ import epermit.services.AuthorityService;
 import epermit.services.ConfigService;
 import lombok.RequiredArgsConstructor;
 
+@CrossOrigin
 @RestController
 @RequiredArgsConstructor
 @RequestMapping
